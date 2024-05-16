0f6f2aecc95d00de4de2d988a1283cbd24a51d73
duyenleICD <duyenlh@icd-vn.com>
2024-05-16 10:09:57 +0700

    add

--- app/Http/Controllers/DomainsController.php
+++ app/Http/Controllers/DomainsController.php
@@ -2,84 +2,182 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categories;
 use App\Models\Domains;
 use Illuminate\Http\Request;
 
 class DomainsController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * Display Categories page.
+     * 
+     * @return Renderable
      */
     public function index()
     {
-        //
+        $this->data['categories'] = Categories::all();
+        return view('admin.domains.index', $this->data);
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
+     * Display Categories creation page.
+     * 
+     * @return Renderable
      */
     public function create()
     {
-        //
+        $this->data['categoriesList'] = Categories::arrayForSelect();
+        $this->data['is_validate'] = Categories::getValueToSelectIsValidate();
+        $this->data['headline']     = 'Thêm Danh Mục Mới';
+        $this->data['mode']         = 'create';
+
+        return view('admin.domains.form', $this->data);
+    }
+
+    public function store(CategoriesRequest $request)
+    {
+        $formData = $request->all();
+        if (Categories::create($formData)) {
+            Session::flash('message', 'Danh mục đã được thêm thành công');
+        }
+
+        return redirect()->to('categories');
+    }
+
+    public function destroy($id)
+    {
+        if (Categories::find($id)->delete()) {
+            Session::flash('message', 'Danh mục đã được xóa thành công');
+        }
+
+        return redirect()->to('categories');
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
+     * Display Categories Edit page.
+     * 
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function edit($id)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Domains  $domains
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Domains $domains)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Domains  $domains
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Domains $domains)
-    {
-        //
+        $this->data['category'] = Categories::findOrFail($id);
+        $this->data['categoriesList'] = Categories::arrayForSelect();
+        $this->data['is_validate'] = Categories::getValueToSelectIsValidate();
+        $this->data['mode']         = 'edit';
+        $this->data['headline']     = 'Cập nhật danh mục';
+        return view('admin.domains.form', $this->data);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Domains  $domains
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Domains $domains)
+    public function update(CategoriesRequest $request, $id)
     {
-        //
+        $data = $request->all();
+
+        $category = Categories::find($id);
+        $category->name = $data['name'];
+        $category->slug_name = AppHelper::instance()->create_slug($category->name);
+        $category->parent_id = $data['parent_id'];
+        $category->is_validate = $data['is_validate'];
+
+        if ($category->save()) {
+            Session::flash('message', 'Cập nhật danh mục thành công');
+        }
+
+        return redirect()->to('categories');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Domains  $domains
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Domains $domains)
+    public function getSubCategory($categoryId = null)
     {
-        //
+        $categoriesList = [];
+        $categoryId = json_decode($categoryId);
+        if (!$categoryId) {
+            // Case On load page: All level 0 category will be displayed bellow
+            $childCategories = Categories::where('parent_id', '==', 0)->get();
+            $parentCategories = null;
+        } else {
+            $parentCategories = $this->getAllParentIds($categoryId);
+            $childCategories = $this->getAllChildIds($categoryId);
+
+            if (count($parentCategories) == 1) {
+                // Case when user click on a category but there are no child categories belonging to it
+                $categoriesList = Categories::where('parent_id', '==', 0)->get();
+                $categoryId = $parentCategories[0]->id;
+            } else if (empty($childCategories)) {
+                $parentCategories = array_filter($parentCategories, function ($item) use ($categoryId) {
+                    return $item->id !== intval($categoryId);
+                });
+                $lastEle = end($parentCategories);
+                $parentCategories = $this->getAllParentIds($lastEle->id);
+                // if (count($parentCategories) == 1) {
+                //     $categoriesList = Categories::where('parent_id', '==', 0)->get();
+                // }
+                $childCategories = $this->getAllChildIds($lastEle->id);
+            }
+        }
+
+
+        return view('home.partials.sub_categories')->with([
+            'parentCategories' => $parentCategories,
+            'childCategories' => $childCategories,
+            'categoriesList' => $categoriesList,
+            'selectedCategory' => $categoryId,
+        ]);
+    }
+
+    public function getAllParentIds($categoryId, &$parentCategories = [])
+    {
+        $category = Categories::where('id', $categoryId)
+            ->first();
+
+        if ($category) {
+            $parentObject = new stdClass();
+            $parentObject->id = $category->id;
+            $parentObject->name = $category->name;
+            array_unshift($parentCategories, $parentObject);
+
+            if ($category->parent_id !== 0) {
+                $this->getAllParentIds($category->parent_id, $parentCategories);
+            }
+        }
+        return $parentCategories;
+    }
+
+    public function getAllChildIds($categoryId, &$childCategories = [])
+    {
+        $category = Categories::where('id', $categoryId)
+            ->first();
+
+        if ($category) {
+            $children = $category->children;
+
+            foreach ($children as $child) {
+                $childObject = new stdClass();
+                $childObject->id = $child->id;
+                $childObject->name = $child->name;
+                $childCategories[] = $childObject;
+                $this->getAllChildIds($child->id, $childCategories);
+            }
+        }
+
+        return $childCategories;
+    }
+
+
+    public function getParentCategory($parentCategoryId)
+    {
+        $categoriesList = Categories::all();
+        $selectedCategory = Categories::where('id', $parentCategoryId)->first();
+
+        return view('home.partials.categories_select')->with([
+            'selectedCategory' => $selectedCategory->id,
+            'categoriesList' => $categoriesList
+        ]);
     }
 }
---
+++ resources/views/admin/domains/index.blade.php
@@ -0,0 +1,67 @@
+@auth
+@if(auth()->user()->role == 1)
+@extends('admin.index')
+
+@section('main_content')
+<div class="row clearfix page_header">
+	<div class="col-md-6">
+		<h2> WebSite </h2>
+	</div>
+	<div class="col-md-6 text-right">
+		<a class="btn btn-info" href="{{ url('categories/create') }}"> <i class="fa fa-plus"></i> Thêm Website </a>
+	</div>
+</div>
+
+<!-- DataTales Example -->
+<div class="card shadow mb-4">
+	<div class="card-body">
+		<div class="table-responsive">
+			<table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>Tên</th>
+						<th>Danh mục phụ thuộc</th>
+						<th>Được chọn</th>
+						<th class="text-center">#</th>
+					</tr>
+				</thead>
+				<tfoot>
+					<tr>
+						<th>ID</th>
+						<th>Tên danh mục</th>
+						<th>Danh mục phụ thuộc</th>
+						<th>Được chọn</th>
+						<th class="text-center">#</th>
+					</tr>
+				</tfoot>
+				<tbody>
+					@foreach ($categories as $category)
+					<tr>
+						<td> {{ $category->id }} </td>
+						<td> {{ $category->name }} </td>
+						<td> {{ $category->parent->name }} </td>
+						<td> {{ $category->getValueToSelectIsValidate()[$category->is_validate] }} </td>
+						<td class="text-center">
+							<form method="POST" action=" {{ route('categories.destroy', ['category' => $category->id]) }} ">
+								<a class="btn btn-primary btn-sm" href="{{ route('categories.edit', ['category' => $category->id]) }}">
+									<i class="fa fa-edit"></i>
+								</a>
+								@csrf
+								@method('DELETE')
+								<button onclick="return confirm('Bạn có chắc chắn muốn xóa?')" type="submit" class="btn btn-danger btn-sm">
+									<i class="fa fa-trash"></i>
+								</button>
+							</form>
+						</td>
+					</tr>
+					@endforeach
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
+
+@stop
+@endif
+@endauth
